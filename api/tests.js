c40520f87e1f8d6ae64fb4a850b219ecf6377ef1
kevinwmeier <kevin@wiredmeier.com>
2026-03-04 13:29:48 -0800

    Delete api/tests.js

--- api/tests.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const key = process.env.ANTHROPIC_API_KEY;
-  res.json({
-    hasKey: !!key,
-    keyLength: key ? key.length : 0,
-    keyStart: key ? key.substring(0, 10) : "none",
-  });
-}
